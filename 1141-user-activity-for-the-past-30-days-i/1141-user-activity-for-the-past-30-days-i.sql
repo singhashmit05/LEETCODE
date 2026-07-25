f0497281f440377aef280207c953f801c1e98b7a
ashmit singh <ashysingh005@gmail.com>
2026-07-25 19:11:27 +0530

    Time: 789 ms (5.32%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select  activity_date as day , count(DISTINCT user_id) as active_users
+from activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+group by activity_date
+
+
+
+
+
+
+
+
+
+
+
